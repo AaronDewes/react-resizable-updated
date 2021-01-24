9350e928edb621de1f216de046a8a5f85650f74f
Aaron Dewes <aaron.dewes@web.de>
2021-01-24 15:06:40 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     }
   },
   module: {
-    rules: [
+    options: [
       {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader?cacheDirectory=true'},
     ]
   },
